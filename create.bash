b5bade62ef4d14cb119b5e7189ed168a57a6e7e1
Laclac <ngochths150417@fpt.eedu.vn>
2023-02-21 23:48:59 +0700

    add basion host

---
+++ create.bash
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name project2 --region ap-southeast-1 --template-body file://project-test.yml --parameters file://server-parameters.json --profile todoApp --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
